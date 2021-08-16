59ef3518e3314a38b29f3b2afe60f43317b93727
Valentina Sulbaran <vjss05@gmail.com>
2021-08-16 14:07:22 +0200

    daily exercise

--- exercises.js
+++ exercises.js
@@ -1,3 +1,6 @@
+// 1. FAKENEWS!
+// count the number of fake news within the array.
+
 /* function fakeNews(arg) {
     let fake = 0;
         for (let i = 0; i < arg.length; i++) {
@@ -12,6 +15,24 @@ const fakeNews = arr => "There is " + arr.filter(arg => arg === false).length + 
 
 const news = [true, false, false, true, false, false, false, true, true, true]; 
 console.log(fakeNews(news))
+
+// 2. THE ECHO!
+// Returns the same sentence minus two letters until only the first letter is left.
+
+function checkEcho(str) {
+    if (str.length === 0) {
+    return '';
+    } else {
+        str = str.slice(0, -2);
+        console.log(str);
+        checkEcho(str);
+    }
+    
+}
+  
+   
+const yodel = "Yodelayheeeeehooooo";
+console.log(checkEcho(yodel));
 
 
 
