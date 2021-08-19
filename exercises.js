ee8b9776c9fe5ab05590f138ba77ff2ef116421a
Valentina Sulbaran <vjss05@gmail.com>
2021-08-19 22:40:53 +0200

    daily exercise

--- exercises.js
+++ exercises.js
@@ -160,8 +160,13 @@ const merge = (...args) => {
     });
     return Obj;
 };
-  
 
 console.log(merge({ a: 3, b: 2 }, { a: 2, c: 4 })); // { a: 3, b: 2, c: 4 }
 console.log(merge({ a: 3, b: 2 }, { a: 2, c: 4 }, { e: 8, c: 5})); // { a: 3, b: 2, c: 4, e: 8 }
-  
+
+
+
+// 9. INVERT
+// Returns a new object with the values of the passed object as properties and the keys as values.
+
+
