2592dca2edbb3ca74d89fbc73e3582f4dc7db484
Valentina Sulbaran <vjss05@gmail.com>
2021-09-15 18:36:23 +0200

    daily exercise

--- exercises.js
+++ exercises.js
@@ -182,7 +182,6 @@ console.log(invert({ a: 3, b: 2 }))// { 2: 'b', 3: 'a' }
 
 
 
-
 // 10. Min, Max, Length and Average
 // Return the smallest and highest value of an array as well as its length and its average value.
 
