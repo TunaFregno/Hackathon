dfd9dad7169b7fff72984f5a359f47a36fba506b
Valentina Sulbaran <vjss05@gmail.com>
2021-10-21 18:22:29 +0200

    dailyexercise

--- exercises.js
+++ exercises.js
@@ -496,7 +496,7 @@ console.log(allPairs([4, 5, 1, 3, 6, 8], 9))
 
 // 20. TIC TAC TOE
 
-function ticTacToe(arr) {
+const ticTacToe = (arr) => {
   let winningComb = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
   let combs = [];
   for (let list in arr) {
